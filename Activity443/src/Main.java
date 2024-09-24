0b6375552d82f1562fead17da1bb7ab4dd594fba
andreselianor <113841548+andreselianor@users.noreply.github.com>
2024-09-24 23:08:04 +0200

    folder clean

--- Activity443/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Ejercicio 4.4.3");
-        System.out.println("File type and data");
-
-        TypeFinder app = new TypeFinder();
-        app.FileWork();
-    }
-}
