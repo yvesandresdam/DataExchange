c68c57b255cd48d898dd3b626752c60840ff8be1
Yves Andres <andreselianordam@mail.com>
2024-09-30 19:05:58 +0200

    update DA

---
+++ Activity541/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Application app = new Application();
+        app.launch();
+    }
+}
+
+/*
+    DOCUMENTATION
+    -------------
+
+    Write a program to create a notepad. The program will ask for sentences
+    to the user and write it to a file. The file can exist or not, if it does, the user
+    should be asked whether to overwrite the information or append it. Lines
+    will be numbered sequentially. You may need to read point 6 first.
+
+ */
