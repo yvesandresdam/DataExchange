504bed2e1de54d341c2a9119c166d89cffc0caab
Yves Andres <andreselianordam@mail.com>
2024-10-08 15:05:44 +0200

    update clase martes

--- Activity711/src/Contact.java
+++
@@ -1,8 +0,0 @@
-import java.io.Serializable;
-public class Contact implements Serializable {
-    private String name;
-    private String surname;
-    private String email;
-    private String phone;
-    private String description;
-}
